c20b85d885f5c94b7a49003d92af7c7b7f3d66b9
fl0v <7046246+fl0v@users.noreply.github.com>
2020-06-03 09:44:37 +0300

    planets.stats.user.js: population, soldiers, ground, orbit

--- planets.stats.user.js
+++ planets.stats.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Dark Galaxy - Planets stats
 // @namespace    https://darkgalaxy.com/
-// @version      0.2
+// @version      0.3
 // @description  All your planet are belong to us
 // @author       [2P]DraghasYesterday & Biggy
 // @match        https://beta.darkgalaxy.com/planets/
@@ -11,7 +11,9 @@
 (function() {
 
     const resPattern = /([\d,]+)\s+\(([\+\d,]+)\)\s+([\d%]+)/; // will split resource data ex: '52,126 (+3,465) 70%'
-    const parseValue = (v) => { return parseInt(v.replace(/[,\+%]+/g, '')) }; // will normalize a value to be able to use it in Math operation '52,126' -> 52126; '+3,465' -> 3465; '70%' -> 70
+    const popPattern = /([\d,]+)\s+\(([\+\d,]+)\soccupied\)/; // will split population  data ex: '52,126 (5,000 occupied)'
+    const othPattern = /([\d,]+)/; // simple value
+    const parseValue = (v) => { return parseInt(String(v).replace(/[,\+%]+/g, '')) }; // will normalize a value to be able to use it in Math operation '52,126' -> 52126; '+3,465' -> 3465; '70%' -> 70
     const formatNumber = (value) => { return value.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,'); }; // same format as the rest of the values in ui
 
     /**
@@ -19,14 +21,24 @@
      */
     const resTotal = function (xpath) {
         let total = {
-            collected: 0,
+            collected: 0, // res or pop (including occupied)
             production: 0,
             perc: 0,
+            avg: 0, // used for avg pop
             count: 0
         };
         total = Array.from(document.querySelectorAll(xpath))
         .reduce(function (carry, el) {
-            const [, collected, production, perc] = el.innerText.match(resPattern);
+            var val = el.innerText;
+            var collected, production, perc;
+            if (resPattern.test(val)) {
+                [, collected, production, perc] = val.match(resPattern);
+            } else if (popPattern.test(val)) {
+                let [, iddle, occupied] = val.match(popPattern);
+                collected = parseValue(iddle) + parseValue(occupied);
+            } else if (othPattern.test(val)) {
+                [,collected] = val.match(othPattern);
+            }
             return {
                 collected: carry.collected + parseValue(collected),
                 production: carry.production + parseValue(production),
@@ -34,7 +46,10 @@
                 count: carry.count + 1
             }
         }, total);
-        total.perc = (total.perc / total.count).toFixed(2);
+        if (total.count > 0) {
+            total.perc = (total.perc / total.count).toFixed(2);
+            total.avg = (total.collected / total.count).toFixed(2);
+        }
         return total;
     };
 
@@ -42,27 +57,46 @@
      * Will produce resource template
      */
     const formatResource = (total,code) => {
+        let info = '';
+        if (code == 'worker') {
+            info = formatNumber(total.collected) +' <i>(AVG: '+formatNumber(total.avg)+')</i>';
+        } else if (code == 'soldier' || code == 'ground' || code == 'orbit') {
+            info = formatNumber(total.collected);
+        } else { // resource
+            info = formatNumber(total.collected) +' (+'+formatNumber(total.production)+') AVG: '+ formatNumber(total.perc)+'%';
+        }
         return '<div class="left seperatorRight">'
             + '<img src="/images/units/small/'+code+'.gif" title="'+code+'">'
             + '</div>'
-            + '<span>'
-            + formatNumber(total.collected) +' (+'+formatNumber(total.production)+') AVG '+ formatNumber(total.perc)+'%'
-            + '</span>'
+            + '<span>' + info + '</span>'
         ;
     };
 
     const metal = resTotal('.resource.metal');
     const mineral = resTotal('.resource.mineral');
     const energy = resTotal('.resource.energy');
+    const pop = resTotal('.resource.population');
+    const sold = resTotal('.resource.soldier');
+    const ground = resTotal('.resource.ground');
+    const orbit = resTotal('.resource.orbit');
     //const food = resTotal('.resource.food'); // i miss food :((
 
     const container = document.querySelector('#planetList');
     container.insertAdjacentHTML('afterbegin',
-        '<div class="opacDarkBackground lightBorder seperator planetStats">'
+        '<div class="opacDarkBackground lightBorder ofHidden seperator planetStats">'
             +'<div class="header border">Total resources</div>'
-            +'<div class="resource metal">'+formatResource(metal,'metal')+'</div>'
-            +'<div class="resource mineral">'+formatResource(mineral,'mineral')+'</div>'
-            +'<div class="resource energy">'+formatResource(energy,'energy')+'</div>'
+            +'<div class="left">'
+                +'<div class="resource metal">Metal '+formatResource(metal,'metal')+'</div>'
+                +'<div class="resource mineral">Mineral '+formatResource(mineral,'mineral')+'</div>'
+                +'<div class="resource energy">Energy '+formatResource(energy,'energy')+'</div>'
+            +'</div>'
+            +'<div class="right d-flex">'
+                +'<div class="resource population">Pop '+formatResource(pop,'worker')+'</div>'
+                +'<div class="resource solider">Sol '+formatResource(sold,'soldier')+'</div>'
+                +'<div class="resource ground">Ground '+formatResource(ground,'ground')+'</div>'
+                +'<div class="resource orbit">Orbit '+formatResource(orbit,'orbit')+'</div>'
+            +'</div>'
+
         +'</div>'
     );
 
@@ -71,7 +105,7 @@
      */
     const style = document.createElement('style');
           style.type = 'text/css';
-          style.innerHTML = '.planetStats .resource { height:auto; }';
+          style.innerHTML = '.planetStats .resource { height:auto; } .d-flex { display:flex; white-space: nowrap; } .d-flex-grow { flex-grow:1; } .d-flex-wrap { flex-wrap:wrap; }';
     document.getElementsByTagName('head')[0].appendChild(style);
 
 })();
